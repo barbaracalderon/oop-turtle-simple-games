436027367e68150baab9cec0c9e4e919472f1435
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-02-19 23:25:31 -0300

    Added clear screen method

--- sketch-app/main.py
+++ sketch-app/main.py
@@ -4,22 +4,28 @@ jackie = Turtle()
 screen = Screen()
 
 def move_forward():
-    jackie.forward(20)
+    jackie.forward(10)
 
 def move_left():
-    jackie.left(90)
+    jackie.left(10)
 
 def move_right():
-    jackie.right(90)
+    jackie.right(10)
 
 def move_backward():
-    jackie.backward(20)
+    jackie.backward(10)
+
+def clear_screen():
+    jackie.home()
+    jackie.clear()
+
 
 screen.listen()
 screen.onkey(fun=move_forward, key="w")
 screen.onkey(fun=move_left, key="a")
 screen.onkey(fun=move_right, key="d")
 screen.onkey(fun=move_backward, key="s")
+screen.onkey(fun=clear_screen, key="c")
 
 
 screen.exitonclick()
